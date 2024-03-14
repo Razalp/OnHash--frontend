cb1666450178e1a593b17cbbff061aa7631c18e1
Razalp <razalp0012300@gmail.com>
2024-03-14 11:24:44 +0530

    fixing

--- src/userSide/Chat/Chat.tsx
+++ src/userSide/Chat/Chat.tsx
@@ -65,9 +65,7 @@ const Chat = () => {
 
             socket.on('chat message', (msg: any) => {
                 setMessages((prevMessages:string) => [msg, ...prevMessages]);
-                if (selectedUser && selectedUser.userId) {
-                    setTimeout(fetchMessages, 1000);
-                }
+        
             });
         }
   
